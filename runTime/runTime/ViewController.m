ae9ec2c8a13887b38782feea6c4d676ea4927732
xuliran33 <476883317@qq.com>
2019-05-31 15:32:00 +0800

    runtime的应用及参考文档

--- runTime/runTime/ViewController.m
+++ runTime/runTime/ViewController.m
@@ -8,9 +8,42 @@
 
 /**
  * runtime 的应用
+ * 关联对象(Objective-C Associated Objects)给分类增加属性
+ * 方法魔法(Method Swizzling)方法添加和替换和KVO实现
+ * 消息转发(热更新)解决Bug(JSPatch)
+ * 实现NSCoding的自动归档和自动解档
+ * 实现字典和模型的自动转换(MJExtension)
+ * 参考文档 https://www.jianshu.com/p/6ebda3cd8052
  */
 #import "ViewController.h"
 #import <objc/runtime.h>
+
+/**
+ * 实现UIView的Category添加自定义属性defaultColor
+ */
+
+@interface UIView (DefaultColor)
+
+@property (nonatomic, strong) UIColor *defaultColor;
+
+@end
+
+@implementation UIView (DefaultColor)
+
+@dynamic defaultColor;
+static char kDefaultColorKey;
+
+- (void)setDefaultColor:(UIColor *)defaultColor{
+    objc_setAssociatedObject(self, &kDefaultColorKey, defaultColor, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (UIColor *)defaultColor{
+    return objc_getAssociatedObject(self, &kDefaultColorKey);
+}
+
+
+
+@end
 
 @interface Person : NSObject
 
@@ -31,10 +64,48 @@
 
 @implementation ViewController
 
+/**
+ * 实现替换viewdDidLoad的方法
+ * swizzling 应该只在+load方法中完成
+ *  在 Objective-C 的运行时中，每个类有两个方法都会自动调用。+load 是在一个类被初始装载时调用，+initialize 是在应用第一次调用该类的类方法或实例方法前调用的。两个方法都是可选的，并且只有在方法被实现的情况下才会被调用。
+ * swizzling应该只在dispatch_once 中完成,由于swizzling 改变了全局的状态，所以我们需要确保每个预防措施在运行时都是可用的。原子操作就是这样一个用于确保代码只会被执行一次的预防措施，就算是在不同的线程中也能确保代码只执行一次。Grand Central Dispatch 的 dispatch_once满足了所需要的需求，并且应该被当做使用swizzling 的初始化单例方法的标准。
+ */
++ (void)load{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class class = [self class];
+        SEL originalSelector = @selector(viewDidLoad);
+        SEL swizzledSelector = @selector(jkViewDidLoad);
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzleMethod = class_getInstanceMethod(class, swizzledSelector);
+        
+        // 判断swizzleMethod方法实现是否存在
+        BOOL didAddMethod = class_addMethod(class, originalSelector, method_getImplementation(swizzleMethod), method_getTypeEncoding(swizzleMethod));
+        // 如果swizzleMethod已经存在
+        if (didAddMethod) {
+            class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+        }else{
+            method_exchangeImplementations(originalMethod, swizzleMethod);
+        }
+    });
+}
+
+- (void)jkViewDidLoad{
+    
+    NSLog(@"替换的方法");
+    [super viewDidLoad];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSLog(@"自带的方法");
     // Do any additional setup after loading the view.
     [self performSelector:@selector(foo)];
+    
+    UIView *test = [UIView new];
+    test.defaultColor = [UIColor blackColor];
+    NSLog(@"text.defaultColor : %@", test.defaultColor);
 }
 
 /**
